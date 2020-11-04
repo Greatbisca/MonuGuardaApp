7bb1d95c4c87fe596238fdd6388a5986b5e6a32a
Greatbisca <61785534+Greatbisca@users.noreply.github.com>
2020-11-04 18:02:55 +0000

    Implementação do Template

--- MonuGuardaApp/Program.cs
+++ MonuGuardaApp/Program.cs
@@ -15,7 +15,6 @@ namespace MonuGuardaApp
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
