64d7ea812bf5a9cf8ed7276ed27083a9cfdde86f
JElmanoAF <joao515515@hotmail.com>
2020-12-11 10:01:09 +0000

    Alterações nas visitas guiadas (Correções de nomes, mensagens de erro adicionadas e organização das paginas)

--- MonuGuardaApp/Models/VisitasGuiadas.cs
+++ MonuGuardaApp/Models/VisitasGuiadas.cs
@@ -9,16 +9,28 @@ namespace MonuGuardaApp.Models
     public class VisitasGuiadas
     {
         public int VisitasGuiadasId { get; set; }
+
+        [Required(ErrorMessage = "É necessário colocar o seu Id")]
         public int GuiaId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "É necessário colocar nome")]
         [StringLength(100)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "É necessário colocar o local de partida")]
         public string LocalPartida { get; set; }
+
+        [Required(ErrorMessage = "É necessário colocar o local de chegada")]
         public string LocalChegada { get; set; }
+
+        [Required(ErrorMessage = "É necessário colocar a data da visita guiada")]
         public DateTimeOffset DataVisita { get; set; }
         public string Morada { get; set; }
+
+        [Required(ErrorMessage = "É necessário colocar número de telemóvel")]
         public int Telemovel { get; set; }
+
+        [Required(ErrorMessage = "É necessário colocar o número maxímo de pessoas")]
         public int NMaxPessoas { get; set; }
         public bool Completo { get; set; }
     }
