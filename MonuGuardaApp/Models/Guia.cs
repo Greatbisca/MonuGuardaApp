5ecf2e03f450c8945169c5ad52e119fab3b9bb5e
Francisco Marques <francisco_marques00@hotmail.com>
2021-01-15 09:47:57 +0000

    Criação mensagens de erro

--- MonuGuardaApp/Models/Guia.cs
+++ MonuGuardaApp/Models/Guia.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,20 @@ namespace MonuGuardaApp.Models
     public class Guia
     {
         public int GuiaId { get; set; }
+
+        [Required(ErrorMessage = "É necessário colocar o nome")]
+        [StringLength(20)]
         public string Nome { get; set; }
+
+        [StringLength(9)]
         public int Telemovel { get; set; }
 
+        [Required(ErrorMessage = "É necessário colocar o email")]
         public string Email { get; set; }
 
         public DateTime DataDeNascimento { get; set; }
 
+        [Required(ErrorMessage = "É necessário colocar a morada")]
         public string Morada { get; set; }
     }
 }
