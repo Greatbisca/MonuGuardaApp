c125a2bef8723b0b73d528cafb031ec3385b5663
Greatbisca <61785534+Greatbisca@users.noreply.github.com>
2021-01-27 15:23:47 +0000

    Permissoes para as Reservas

--- MonuGuardaApp/Controllers/ReservaVisitasController.cs
+++ MonuGuardaApp/Controllers/ReservaVisitasController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,8 +11,9 @@ using MonuGuardaApp.Models;
 
 namespace MonuGuardaApp.Controllers
 {
-    public class ReservaVisitasController : Controller
-    {
+    [Authorize(Roles = "Admin, Turista")]
+        public class ReservaVisitasController : Controller
+        {
         private readonly MonuGuardaAppContext _context;
 
         public ReservaVisitasController(MonuGuardaAppContext context)
